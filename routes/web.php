971b9371028740b557c3d2ad4e67501929b71e12
jun-alfajr <jun.fajr@gmail.com>
2020-11-08 15:47:44 +0700

    auth admin n user

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\AboutUsComponent;
 use App\Http\Livewire\ContactUsComponent;
+use App\Http\Livewire\User\UserDashboardComponent;
+use App\Http\Livewire\Admin\AdminDashboardComponent;
 use Illuminate\Support\Facades\Route;
 
 
@@ -20,3 +22,17 @@ Route::get('/checkout', CheckoutComponent::class);
 Route::get('/about-us', AboutUsComponent::class);
 
 Route::get('/contact-us', ContactUsComponent::class);
+
+// Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//     return view('dashboard');
+// })->name('dashboard');
+
+// User or Customer
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::get('/user/dashboard', UserDashboardComponent::class)->name('user.dashboard');
+});
+
+// Admin
+Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function () {
+    Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
+});
