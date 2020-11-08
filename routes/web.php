19b0a6ede82d6be4e1d72111d95758102e2d402f
jun-alfajr <jun.fajr@gmail.com>
2020-11-08 14:24:08 +0700

    add pages

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,22 @@
 <?php
 
 use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\AboutUsComponent;
+use App\Http\Livewire\ContactUsComponent;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
 Route::get('/', HomeComponent::class);
+
+Route::get('/shop', ShopComponent::class);
+
+Route::get('/cart', CartComponent::class);
+
+Route::get('/checkout', CheckoutComponent::class);
+
+Route::get('/about-us', AboutUsComponent::class);
+
+Route::get('/contact-us', ContactUsComponent::class);
